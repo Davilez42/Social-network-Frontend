5094333928a4b10d27933c5bd0125ef635b2531c
YefersonA <yeferson.aguiar@correounivalle.edu.co>
2023-11-05 11:55:30 -0500

    mejore la barra de navegacion navside

--- src/components/feed/navbarside/NavBarSide.jsx
+++ src/components/feed/navbarside/NavBarSide.jsx
@@ -1,19 +1,38 @@
 import React from "react";
+import { FaUser, FaUsers, FaEnvelope,FaHandshake,FaCog} from 'react-icons/fa';
 import "./navbarside.css";
 
 export default function NavBarSide() {
+  const numPublicaciones = 5; 
+  const numSeguidores = 100; 
+  const numSeguidos = 50;
+
+  
   return (
     <div className="container-navbar-side">
-    <h1>Navigate</h1>
-    {/*agregar enlaces a diferentes secciones*/}
-    <nav>
-    <ul>
-      <li><a href="/perfil">✔ Perfil</a></li>
-      <li><a href="/amigos">✔ Amigos</a></li>
-      <li><a href="/mensajes">✔ Mensajes</a></li>
-      <li><a href="/grupos">✔ Grupos</a></li>
-    </ul>
-    </nav>
-  </div>
-);
+      <div className="avatar">
+        {/*  agregar AVATAR*/}
+        <span className="avatar-content"></span>
+      </div>
+      <div className="nombre-persona">
+        <p> RICARDO DARIO</p>
+      </div>
+      
+      <div className="seccion-perfil">
+        <li>Publicaciones {numPublicaciones}</li>
+        <li>Seguidores {numSeguidores}</li>
+        <li>Seguidos {numSeguidos}</li>
+         
+      </div>
+      <nav>
+        <ul>
+          <li><a href="/perfil"><FaUser size={18}/>  Perfil</a></li>
+          <li><a href="/amigos"><FaUsers size={18}/>  Amigos</a></li>
+          <li><a href="/mensajes"><FaEnvelope size={18}/>  Mensajes</a></li>
+          <li><a href="/grupos"><FaHandshake size={18}/>  Grupos</a></li>
+          <li><a href="/configuracion"><FaCog size={18}/>  Configuracion</a></li>
+        </ul>
+      </nav>
+    </div>
+  );
 }
