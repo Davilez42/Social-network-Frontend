26f50cd3e245da3da3b581ed501768fff9df2c00
YefersonA <yeferson.aguiar@correounivalle.edu.co>
2023-11-05 09:55:43 -0500

    agregue barra de navegacion

--- src/components/feed/navbarside/NavBarSide.jsx
+++ src/components/feed/navbarside/NavBarSide.jsx
@@ -1,10 +1,19 @@
+import React from "react";
 import "./navbarside.css";
+
 export default function NavBarSide() {
   return (
-    <>
-      <div className="container-navbar-side">
-        <h1>NavbarSide</h1>
-      </div>
-    </>
-  );
+    <div className="container-navbar-side">
+    <h1>Navigate</h1>
+    {/*agregar enlaces a diferentes secciones*/}
+    <nav>
+    <ul>
+      <li><a href="/perfil">✔ Perfil</a></li>
+      <li><a href="/amigos">✔ Amigos</a></li>
+      <li><a href="/mensajes">✔ Mensajes</a></li>
+      <li><a href="/grupos">✔ Grupos</a></li>
+    </ul>
+    </nav>
+  </div>
+);
 }
